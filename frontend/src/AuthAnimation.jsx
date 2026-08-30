1e0eab3945eb966111a5beece31c0effbb69a43c
Jayasrip08 <jayasrip1808@gmail.com>
2026-08-30 12:00:47 +0530

    fix: use named export { Lottie } from lottie-react

--- frontend/src/AuthAnimation.jsx
+++ frontend/src/AuthAnimation.jsx
@@ -1,4 +1,4 @@
-import Lottie from 'lottie-react';
+import { Lottie } from 'lottie-react';
 import loginAnimation from './animations/login.json';
 import signupAnimation from './animations/signup.json';
 

09641017776a64936e8e6419c65e45f1132f6a13
Jayasrip08 <jayasrip1808@gmail.com>
2026-09-03 15:04:51 +0530

    Enable media file serving route in production mode

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.views.static import serve
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -31,8 +32,10 @@ urlpatterns = [
     path('api/payments/', include('payments.urls')),
     path('api/analytics/', include('analytics_app.urls')),
     path('api/support/', include('notifications.urls')),
+
+    # Media File Serving Route (Production Enabled)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
